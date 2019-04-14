b110c1e19724657bbb8efbeb454923fc4571d1f3
joao barbirato <joaobarbirato@gmail.com>
2019-04-14 14:17:56 -0300

    AMR visualization module added

---
+++ app/amr_module/controllers.py
@@ -0,0 +1,25 @@
+import json
+from flask import Blueprint, render_template, request
+
+from app import app
+from app.src.UTIL.amr_networkx import crawl_link_to_generated_amr
+
+mod_amr = Blueprint('amr', __name__, url_prefix='/amr')
+
+
+@mod_amr.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('amr_module/index.html', button="Gerar AMRs")
+
+
+@mod_amr.route('/generate', methods=['GET', 'POST'])
+def generate():
+    _link = request.form['link']
+
+    dict_return = crawl_link_to_generated_amr(_link)
+
+    return app.response_class(
+        response=json.dumps(dict_return),
+        status=200,
+        mimetype='application/json'
+    )
