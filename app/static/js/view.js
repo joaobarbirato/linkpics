d024a90eb717eca547e8207a83da1c962c3d5156
joao barbirato <joaobarbirato@gmail.com>
2019-06-05 16:13:16 -0300

    Added check function for every batch in view

--- app/static/js/view.js
+++ app/static/js/view.js
@@ -14,6 +14,21 @@ $('#check-all').click(function() {
     }
 });
 
+$('.check-batch').each(function(index, item){
+    $(item).click(function () {
+        const checked = $(this).prop('checked');
+        const this_id = $(this).prop('id');
+        const this_label_sel = '#label-' + this_id;
+        msg = $(this_label_sel).html();
+        $("#div-of-" + this_id + ' .checkbox').prop('checked', checked);
+        if(checked){
+            $(this_label_sel).html(msg.replace("Marcar", "Desmarcar"));
+        }else{
+            $(this_label_sel).html(msg.replace("Desmarcar", "Marcar"));
+        }
+    });
+});
+
 function validateSelect(){
     console.log(select_input.val() !== "");
     return (select_input.val() !== "");
